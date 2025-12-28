6f4a75dab1ac914626e6bbe0529e0c801bd19bd6
Felipe Macêdo <felipbbmacedo@gmail.com>
2025-12-27 21:05:21 -0300

    retirado comentário de trecho de código

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,11 +1,11 @@
 public class Principal {
     public static void main(String[] args) {
-//        ConfiguracaoAPP d1, d2;
-//        d1 = ConfiguracaoAPP.getInstance();
-//        d2 = ConfiguracaoAPP.getInstance();
-//        System.out.println(d1);
-//        System.out.println(d2);
-// ----------------------------------
+        ConfiguracaoAPP d1, d2;
+        d1 = ConfiguracaoAPP.getInstance();
+        d2 = ConfiguracaoAPP.getInstance();
+        System.out.println(d1);
+        System.out.println(d2);
+ ----------------------------------
     Notificacao notificacao = NotificacaoFactory.criarNotificacao("WHATSAPP");
     notificacao.enviar("Olá! Mensagem enviada!");
 
